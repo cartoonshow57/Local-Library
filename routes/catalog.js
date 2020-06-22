eae5e71bac8144c5968eab6f7b1ce3d445f57570
Cartoon <cartoonshow57@gmail.com>
2020-06-22 13:45:04 +0530

    Updated after fixing errors in models

--- routes/catalog.js
+++ routes/catalog.js
@@ -1,13 +1,14 @@
 var express = require('express');
+var router = express.Router();
 
 var book_controller = require('../controllers/bookController');
 var author_controller = require('../controllers/authorController');
 var genre_controller = require('../controllers/genreController');
 var book_instance_controller = require('../controllers/bookinstanceController');
 
-var router = express.Router();
 
-// Book routes
+// Book Routes
+
 router.get('/', book_controller.index);
 
 router.get('/book/create', book_controller.book_create_get);
@@ -27,7 +28,8 @@ router.get('/book/:id', book_controller.book_detail);
 router.get('/books', book_controller.book_list);
 
 
-// Author routes
+
+// Author Routes
 
 router.get('/author/create', author_controller.author_create_get);
 
@@ -46,17 +48,18 @@ router.get('/author/:id', author_controller.author_detail);
 router.get('/authors', author_controller.author_list);
 
 
-// Genre routes
+
+//Genre Routes
 
 router.get('/genre/create', genre_controller.genre_create_get);
 
 router.post('/genre/create', genre_controller.genre_create_post);
 
-router.get('genre/:id/delete', genre_controller.genre_delete_get);
+router.get('/genre/:id/delete', genre_controller.genre_delete_get);
 
 router.post('/genre/:id/delete', genre_controller.genre_delete_post);
 
-router.get('/genre:id/update', genre_controller.genre_update_get);
+router.get('/genre/:id/update', genre_controller.genre_update_get);
 
 router.post('/genre/:id/update', genre_controller.genre_update_post);
 
@@ -65,7 +68,8 @@ router.get('/genre/:id', genre_controller.genre_detail);
 router.get('/genres', genre_controller.genre_list);
 
 
-// Bookinstance routes
+
+// Bookinstance Routes
 
 router.get('/bookinstance/create', book_instance_controller.bookinstance_create_get);
 
@@ -82,5 +86,6 @@ router.post('/bookinstance/:id/update', book_instance_controller.bookinstance_up
 router.get('/bookinstance/:id', book_instance_controller.bookinstance_detail);
 
 router.get('/bookinstances', book_instance_controller.bookinstance_list);
+
 
 module.exports = router;
