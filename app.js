681399873919a478beb29e485e67fec9b575abd6
Cartoon <cartoonshow57@gmail.com>
2020-06-29 13:13:10 +0530

    Added file changes for Heroku

--- app.js
+++ app.js
@@ -15,7 +15,8 @@ var app = express();
 
 app.use(helmet());
 
-var mongoDB = 'mongodb+srv://Cartoon:helloworld@cluster0-j5zwn.mongodb.net/local_library?retryWrites=true&w=majority';
+var dev_db_url = 'mongodb+srv://Cartoon:helloworld@cluster0-j5zwn.mongodb.net/local_library?retryWrites=true&w=majority';
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error.'));
